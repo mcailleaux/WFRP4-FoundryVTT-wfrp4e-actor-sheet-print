137ae09586875085a3a24ae40f4e73afdd954b51
Matthieu CAILLEAUX <mcailleaux.greenskin@gmail.com>
2021-10-13 00:08:17 +0200

    chore: correct scripts

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@ npm run build
 mkdir -p ./dist/lang
 mkdir -p ./dist/styles
 cp ./lang/* ./dist/lang/
-cp ./src/styles/* ./dist/styles/
+cp ./styles/* ./dist/styles/
 cp module.json ./dist/module.json
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+//ActorSheet
+
+Hooks.on('renderActorSheet', (_app: ActorSheet, _html: JQuery) => {
+  console.dir(_app);
+});
